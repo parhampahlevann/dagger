cdb48cf7d042bd68aa70e325cbe25ca6d9999067
Parham Pahlevan <168536457+parhampahlevann@users.noreply.github.com>
2026-09-11 04:33:09 +0330

    Change default port from 443 to 8443

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ NC='\033[0m'
 
 BINARY="/usr/local/bin/DaggerConnect"
 CONFIG_DIR="/etc/DaggerConnect"
-PORT="443"
+PORT="8443"
 PSK="123"
 
 CONFIG=""
@@ -42,6 +42,13 @@ QM_MTU=""
 QM_BLOCK=""
 CLIENT_CONN_POOL="4"
 PORTS=()
+P=""
+FMT=""
+ACT=""
+CHOICE=""
+CONFIRM=""
+DORESTART=""
+IDX=""
 
 _ts()   { date '+%H:%M:%S'; }
 info()  { echo -e "${DIM}$(_ts)${NC} ${CYAN}[INFO]${NC}  $*"; }
@@ -1184,7 +1191,7 @@ pause() {
 
 while true; do
     clear 2>/dev/null || true
-    echo -e "${CYAN}${BOLD}══ DaggerConnect Manager (Port: 443 | Token: 123 | Offline) ══${NC}\n"
+    echo -e "${CYAN}${BOLD}══ DaggerConnect Manager (Port: 8443 | Token: 123 | Offline) ══${NC}\n"
     echo "  1) Install Server"
     echo "  2) Install Client"
     echo "  3) Service Status"
